eb6294620df65ea027fcd6cfb2e2f65e28a002ef
bispo-eugenio <bispoeugenio270@gmail.com>
2025-06-23 20:39:51 -0300

    feat: adiciona função de validação de url com regex; adiciona is_empty para verificar se string está vazia

--- src/my_package/utils.py
+++ src/my_package/utils.py
@@ -1,2 +1,10 @@
-def validated_link() -> None:
-    pass
+import re
+
+# ========== Funções de Utilidades ==========
+
+def validated_link(url:str) -> bool:
+    pattern = re.compile(r"https?://[a-zA-Z0-9.-]+\.[a-z]{2,}")
+    return pattern.match(url) is not None
+
+def is_empty(string: str) -> bool:
+    return len(string) == 0 
