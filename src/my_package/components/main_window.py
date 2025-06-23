4bcd799cda21e87d2580701b7e748e4830197c3b
bispo-eugenio <bispoeugenio270@gmail.com>
2025-06-23 20:41:33 -0300

    refactor: altera configurações do main_window

--- src/my_package/components/main_window.py
+++ src/my_package/components/main_window.py
@@ -1,19 +1,16 @@
-from PySide6.QtWidgets import (QMainWindow, QWidget, 
-                               QHBoxLayout, QLayout)
+from PySide6.QtWidgets import QMainWindow, QWidget, QGridLayout, QLayout
 from my_package.constants import WIDTH, HEIGTH, STYLE_MAIN_WINDOW
-
-
 
 class MainWindow(QMainWindow):
 
     def __init__(self, parent: QWidget | None = None, *args, **kwargs) -> None: #type: ignore
-        super().__init__(*args, **kwargs) #type: ignore
+        super().__init__(parent, *args, **kwargs) #type: ignore
 
         #Propriedades
         self.central = QWidget()
         self.setCentralWidget(self.central)
-        self._layout_h_box = QHBoxLayout()
-        self.central.setLayout(self._layout_h_box)
+        self._layout_grid_box = QGridLayout()
+        self.central.setLayout(self._layout_grid_box)
 
         #Chamada de métodos 
         self._config_main_window()
@@ -26,12 +23,12 @@ class MainWindow(QMainWindow):
         self.setFixedSize(WIDTH, HEIGTH)
     
     # ========== Métodos Adicionais da Classe ==========
-    
+
     def _style(self) -> None:
         self.setStyleSheet(STYLE_MAIN_WINDOW)
 
-    def add_layout(self, layout: QLayout) -> None:
-        self._layout_h_box.addLayout(layout)
+    def add_layout(self, layout: QLayout, row:int, column:int) -> None:
+        self._layout_grid_box.addLayout(layout, row, column)
 
     def add_widget(self, widget: QWidget) -> None:
-        self._layout_h_box.addWidget(widget)
+        self._layout_grid_box.addWidget(widget)
